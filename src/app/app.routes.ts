81dbc6a60dad1430efb754d04857eb11ad56f7a6
yehudaz <yehuda.zfat@gmail.com>
2024-07-01 18:38:30 +0300

    Auto-generated commit

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-
-export const routes: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./users/users.component').then(m => m.UsersComponent)
-  },
-
-];
